dd573db372c226f227a76957857ad1f6d7197dbc
Camilo Vargas <camilovargasingm@gmail.com>
2021-10-07 11:08:29 -0500

    add component menuto secundario

---
+++ src/components/Header/MenuTopSecund/MenuTopSecund.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./MenuTopSecund.scss"
+
+export default function MenuTopSecund() {
+  return (
+    <ul className="menutopsecund">
+      <li>Replays</li>
+      <li>Highlights</li>
+      <li>Guide</li>
+    </ul>
+  )
+}
