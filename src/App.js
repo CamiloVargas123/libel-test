7fda3e2505938cc70fbd1aefbfd84dda2fd17480
Camilo Vargas <camilovargasingm@gmail.com>
2021-10-07 11:22:23 -0500

    add compoente of classlive

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Header from "./components/Header/Header";
 import Slider from "./components/Slider/Slider";
+import Content from "./components/Content/Content";
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <div className="oneView">
-        <Header />
-        <Slider />
-      </div>
+      <Header />
+      <Slider />
+      <main>
+        <Content />
+      </main>
     </div>
   );
 }
